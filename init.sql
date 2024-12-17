bce3d06df5baf16bc669ee150e0f0471201c839d
wissem.sghaier <wissem.sghaier@artificial-insight.co>
2024-12-17 13:22:39 +0100

    connection database with backend spring-boot

---
+++ init.sql
@@ -0,0 +1,7 @@
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'Fatma123!';
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Fatma123!';
+FLUSH PRIVILEGES;
+
+
+
+
